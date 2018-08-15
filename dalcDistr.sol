4e9044dcc0f2785200c14ee009f23c8a1ef97cd9
eltNEG <yorewole@gmail.com>
2018-08-15 18:07:27 +0100

    comment out 0 balance

--- dalcDistr.sol
+++ dalcDistr.sol
@@ -32,13 +32,13 @@ contract Distributor {
         //Twitter
         Recipients.push(Recipient(0xd90dCe04ea14D33F91fBc114451d881Be8663148, 38.928*1000)); //Dcoollakky247
         Recipients.push(Recipient(0x72b0DE7A3340f204431655243C2235De822E7442, 25.952*1000)); //Yamanbarat55
-        Recipients.push(Recipient(0x5Cae2473E1A7082d10637183898a931bC8A89cf6, 0*1000)); //Sokerz
+        //Recipients.push(Recipient(0x5Cae2473E1A7082d10637183898a931bC8A89cf6, 0*1000)); //Sokerz
         Recipients.push(Recipient(0x899381758746e07C6074a46C6B54d0BB312ea181, 25.952*1000)); //ojosamson
-        Recipients.push(Recipient(0xd6C93A3148140dB082a17C2Bb100E66acaAAEE0b, 0*1000)); //Awaknow
+        //Recipients.push(Recipient(0xd6C93A3148140dB082a17C2Bb100E66acaAAEE0b, 0*1000)); //Awaknow
         Recipients.push(Recipient(0xFd23501DfF368b3aD756350fF9B18e6afB638afD, 25.952*1000)); //joykulot
         Recipients.push(Recipient(0x098E414537A78c322C7FfbB66068e3411B4b981A, 38.928*1000)); //maxinvestor
         Recipients.push(Recipient(0x58ac92F7D03dcd53eA369BFCAD80933d165BdcD5, 25.952*1000)); //Nemoy
-        Recipients.push(Recipient(0xAC360B63d7c00D7A069Cf9ea6Bbd48CCdfB71ABF, 0*1000)); //chicipuci
+        //Recipients.push(Recipient(0xAC360B63d7c00D7A069Cf9ea6Bbd48CCdfB71ABF, 0*1000)); //chicipuci
         Recipients.push(Recipient(0x8EA5697B0555b0Ed91D3f45741d7abe97D8Ca2Df, 25.952*1000)); //alisaddt
         Recipients.push(Recipient(0xa50bF945C446f774A2e2d47c129df061b874dc75, 38.928*1000)); //AnalynCadiz
         Recipients.push(Recipient(0x38b9252759d577e892F6806Ca26655Da77899DAd, 9.732*1000)); //topolan
@@ -72,7 +72,7 @@ contract Distributor {
         Recipients.push(Recipient(0x133e9693907A85230F997daDF3fF219e4946b1CE, 25.952*1000)); //Robert De Crypto
         Recipients.push(Recipient(0xC2775F355F3377293Ffc6CE11ab0841Ce183A44F, 12.976*1000)); //KINGCHACHA1
         Recipients.push(Recipient(0x64aA278Fdc9AC0BC4f6E3a0365753368AA467DBD, 38.928*1000)); //klinohek
-        Recipients.push(Recipient(0xF09e67698bacB9Aad5d9C825FF61a3A1E55568C6, 0*1000)); //katrusia
+        //Recipients.push(Recipient(0xF09e67698bacB9Aad5d9C825FF61a3A1E55568C6, 0*1000)); //katrusia
         Recipients.push(Recipient(0x82a49482Ce070291b7B3F2d38d5fc3581780A4BF, 38.928*1000)); //Kravmaxx
         Recipients.push(Recipient(0xbaBee887566224FB964273Ea117BD0c752Fb9D04, 25.952*1000)); //KriptaBiznes
         Recipients.push(Recipient(0xA850a2Df4245982901Ff86b65dfC24110EEC7Cca, 12.976*1000)); //boynton2857dina557
@@ -83,7 +83,7 @@ contract Distributor {
         Recipients.push(Recipient(0xAc9e3B74135522CC45523F63F0bB53Cfa45cb1CF, 38.928*1000)); //MedvedevVasilyS
         Recipients.push(Recipient(0x906dfd49748DBc3c4309348070760f68bf108776, 38.928*1000)); //nancekapor
         Recipients.push(Recipient(0x7aF4Cb205142a4faC2c100f79258dB06A7B31C24, 38.928*1000)); //Stug
-        Recipients.push(Recipient(0xee98a8C53DBcc402BD3a5627849FFc7aEBd8BEfE, 0*1000)); //Nikos_Anasias
+        //Recipients.push(Recipient(0xee98a8C53DBcc402BD3a5627849FFc7aEBd8BEfE, 0*1000)); //Nikos_Anasias
         Recipients.push(Recipient(0x396629D7bEea698ae897bf230721decaB7C62DBE, 25.952*1000)); //trikovasvetlana
         Recipients.push(Recipient(0x127962b9b5da42856d74ee9d923305dcfae277b8, 25.952*1000)); //slendy1830
         Recipients.push(Recipient(0x50c8D1fE517673dA731090A6792BA2c59508a8A7, 9.732*1000)); //paveleso
@@ -94,13 +94,13 @@ contract Distributor {
         Recipients.push(Recipient(0xA04a151Eb04ba070e776AE011a85deC8F5030101, 38.928*1000)); //Heopu
         Recipients.push(Recipient(0xCDa7A32b1BD9539a509aEAA764E7559219D28aD7, 38.928*1000)); //Maykl
         Recipients.push(Recipient(0x1A441cDEA47f1D0f704f2b26C5F620B46292DBBD, 25.952*1000)); //@Kripto-12
-        Recipients.push(Recipient(0x1A441cDEA47f1D0f704f2b26C5F620B46292DBBD, 0*1000)); //@Kripto
+        //Recipients.push(Recipient(0x1A441cDEA47f1D0f704f2b26C5F620B46292DBBD, 0*1000)); //@Kripto
         Recipients.push(Recipient(0x6bc5759f15FD5f13Edda81743565e893506b4374, 38.928*1000)); //Ruslan84
         Recipients.push(Recipient(0xBf936c8fd9a17bb0141a4a71aF6116Dd6F3B7C69, 38.928*1000)); //kripterst
         Recipients.push(Recipient(0xe189B1950B9Af7D6cd0B48148e68865823402A71, 38.928*1000)); //Alexandr Shenkarenko
         Recipients.push(Recipient(0x7c893f5d3edc9671bc682db9df3ce993c2e520ff, 38.928*1000)); //shilpy275
         Recipients.push(Recipient(0x6Cd20F597DdcB3995A66DE7416D159A88a980367, 38.928*1000)); //harvestnewcoin
-        Recipients.push(Recipient(0x6Cd20F597DdcB3995A66DE7416D159A88a980367, 0*1000)); //harvestnewcoin
+        //Recipients.push(Recipient(0x6Cd20F597DdcB3995A66DE7416D159A88a980367, 0*1000)); //harvestnewcoin
         Recipients.push(Recipient(0x9A5fff48B700cf99a185a1586F1bC74DD0Af9F7C, 38.928*1000)); //SidoraK_T
         Recipients.push(Recipient(0x8Eba7A557c74dDB09d14A04B103CCe8320d79Dc7, 38.928*1000)); //Skaser
         Recipients.push(Recipient(0x394904C294b0FD84a200708b80764c63792EaCB2, 25.952*1000)); //ElenaS
@@ -130,7 +130,7 @@ contract Distributor {
         Recipients.push(Recipient(0xb4Ff7334fE963db0E8f677a6c2Bbb9D3AcE1B580, 29.196*1000)); //Cabella
         Recipients.push(Recipient(0x93f1F825DC6e653f4e0E90D374f093B69163423F, 19.464*1000)); //Saifullahbabu
         Recipients.push(Recipient(0xaa68eCa20F3AadaBFCACF44228962Fbd723e1e01, 19.464*1000)); //Corvin17
-        Recipients.push(Recipient(0xdb213322E0852925017B826B384A583321D33F9E, 0*1000)); //Ssaahw
+        //Recipients.push(Recipient(0xdb213322E0852925017B826B384A583321D33F9E, 0*1000)); //Ssaahw
         Recipients.push(Recipient(0xba5b928a6959c563075174C17F4d361507003a3A, 29.196*1000)); //TatjanaMakar
         Recipients.push(Recipient(0xe08bE7be0Da067a8B35D27A9eFBF86B01fB8A2c4, 29.196*1000)); //ZarinaAhmedjan
         Recipients.push(Recipient(0x0658667E721123b31b480AfD32287055399cdceC, 38.928*1000)); //YurLinOne
@@ -140,10 +140,10 @@ contract Distributor {
         Recipients.push(Recipient(0x0A8223852ce225eA5656bEc7Eb8a071208dA866D, 29.196*1000)); //Starlaaa87
         Recipients.push(Recipient(0x47ea3d8D172790343b60f9F16143bD329DdCC392, 19.464*1000)); //artleks
         Recipients.push(Recipient(0x327052fc98E33480E9dc9Da40E40F5df2E426d9B, 29.196*1000)); //CalvinTran1990
-        Recipients.push(Recipient(0x062138ec5295115C520C5af226c2F2632567eFf2, 0*1000*1000)); //sonali2015
+        //Recipients.push(Recipient(0x062138ec5295115C520C5af226c2F2632567eFf2, 0*1000)); //sonali2015
         Recipients.push(Recipient(0x1c3207E588518EEb560d123b3b9b672DF3Cc3d4e, 19.464*1000)); //Moh ridwan
         Recipients.push(Recipient(0x75F3eD5f2080A1b8bC29d8282e63aa206903115f, 9.732*1000)); //Dek maria
-        Recipients.push(Recipient(0x7d4ce22321121A9d0ebfc214E1CfB59125Cf385C, 0*1000)); //habibsir
+        //Recipients.push(Recipient(0x7d4ce22321121A9d0ebfc214E1CfB59125Cf385C, 0*1000)); //habibsir
         Recipients.push(Recipient(0xA0676eDd171904F489D7E76B554E11E0167397C3, 29.196*1000)); //hidayatgp1
         Recipients.push(Recipient(0xBe52e64Fb68BE2AC3933F96962a234fcE75801dC, 29.196*1000)); //nilbtc4555
         Recipients.push(Recipient(0x0E6CB35Dc823f0cdfbf4801Bbd8fa599B7ee0aD6, 29.196*1000)); //ajaxad
@@ -157,9 +157,9 @@ contract Distributor {
         Recipients.push(Recipient(0x8c117d726bb5109CBbC96B1Ae2B30BBfC65A20f4, 19.464*1000)); //MargoIvanova
         Recipients.push(Recipient(0x52ac2f74e5F7920250d30FBA4131b8c4e6f4597A, 19.464*1000)); //oooecy
         Recipients.push(Recipient(0x9BDbEb5F6E59dd471Bc296B97266D3Ee634B7c7e, 19.464*1000)); //ironman68
-        Recipients.push(Recipient(0xeC72280F671c978484672A5562df07E680b524C1, 0)); //Marmowitz
+        //Recipients.push(Recipient(0xeC72280F671c978484672A5562df07E680b524C1, 0)); //Marmowitz
         Recipients.push(Recipient(0x6c248f43AE613b42448d4EB178E59b06C24F2c83, 29.196*1000)); //sworker
-        Recipients.push(Recipient(0xA621A2E10A88DCDD274e0B2225a10AF8D727950e, 0)); //Gregolev
+        //Recipients.push(Recipient(0xA621A2E10A88DCDD274e0B2225a10AF8D727950e, 0)); //Gregolev
         Recipients.push(Recipient(0xf6c3bFb64FdB7dfFC17E783653E8029EA1635581, 6.488*1000)); //Mstismakov
         Recipients.push(Recipient(0xeC5923Cd130647A70F0ca7De99047b464b19C777, 29.196*1000)); //Chorohon
         Recipients.push(Recipient(0xb5372D530DFB547Adb2e04cbCEa895ceb739B741, 19.464*1000)); //dekcutmanis
@@ -207,7 +207,7 @@ contract Distributor {
         Recipients.push(Recipient(0x10858719531D898EcdAe012b51EAe1CBADf5EB99, 38.4*1000)); //Leodh
         Recipients.push(Recipient(0x03c041db905E0f8dE0b7eCaBb92c775E2b8dE7ff, 38.4*1000)); //dianepot
         Recipients.push(Recipient(0xFd23501DfF368b3aD756350fF9B18e6afB638afD, 38.4*1000)); //joykulot
-        Recipients.push(Recipient(0x5Cae2473E1A7082d10637183898a931bC8A89cf6, 0*1000)); //Sokerz
+        //Recipients.push(Recipient(0x5Cae2473E1A7082d10637183898a931bC8A89cf6, 0*1000)); //Sokerz
         Recipients.push(Recipient(0x73fC0cBbBFD338ac5c205Fec7BC7959E12f8f74a, 38.4*1000)); //toretsk
         Recipients.push(Recipient(0x3A98dAa3dAa51E3B2266A776A5a7E8dc8B735500, 38.4*1000)); //simantobd
         Recipients.push(Recipient(0x58ac92F7D03dcd53eA369BFCAD80933d165BdcD5, 25.6*1000)); //Nemoy
@@ -217,13 +217,13 @@ contract Distributor {
         Recipients.push(Recipient(0xd90dCe04ea14D33F91fBc114451d881Be8663148, 38.4*1000)); //Dcoollakky247
         Recipients.push(Recipient(0x1A441cDEA47f1D0f704f2b26C5F620B46292DBBD, 12.8*1000)); //@Kripto-12
         Recipients.push(Recipient(0xa50bF945C446f774A2e2d47c129df061b874dc75, 38.4*1000)); //AnalynCadiz
-        Recipients.push(Recipient(0x104Ae004Db9ec29e0789F628B11698290Ad73B79, 0*1000)); //Emilere
+        //Recipients.push(Recipient(0x104Ae004Db9ec29e0789F628B11698290Ad73B79, 0*1000)); //Emilere
         Recipients.push(Recipient(0x0649c8c75aea1148b3e51b5f7189ff49ff4b3fd5, 38.4*1000)); //Ivanchailebedev
         Recipients.push(Recipient(0x1322A96779F4Acc28c058579BD712A9CeFd0c835, 38.4*1000)); //bitcoinbaba01
         Recipients.push(Recipient(0x497e11294BE1288d6101F1BdAaaFF03363a39e80, 25.6*1000)); //Bramadi
         Recipients.push(Recipient(0xaa3165Bc7061edA047dd38F64C190d6F3c1Cc0A2, 38.4*1000)); //AlenaShedin
         Recipients.push(Recipient(0xe0bBd84c96042cD7c9f3e52310BeEd1c15621a45, 38.4*1000)); //yepik
-        Recipients.push(Recipient(0xF09e67698bacB9Aad5d9C825FF61a3A1E55568C6, 0*1000)); //katrusia
+        //Recipients.push(Recipient(0xF09e67698bacB9Aad5d9C825FF61a3A1E55568C6, 0*1000)); //katrusia
         Recipients.push(Recipient(0xbaBee887566224FB964273Ea117BD0c752Fb9D04, 38.4*1000)); //KriptaBiznes
         Recipients.push(Recipient(0xd6C6Aa53C1707cBC662a2b79dF54921e711271dE, 38.4*1000)); //Enijesiku Sulaimom
         Recipients.push(Recipient(0xd3199a1B853286d0f64588CC216318fCC24aCeF0, 38.4*1000)); //Zavalishin
@@ -241,7 +241,7 @@ contract Distributor {
         Recipients.push(Recipient(0xd3ee5Bd83dCB85060adB7F45D87dAAFCF96B2f09, 38.4*1000)); //lider777
         Recipients.push(Recipient(0xf3160885190621e460C62229223f39a0f13E64E2, 38.4*1000)); //Reonsver
         Recipients.push(Recipient(0x6a261D32156A26086C3cF77cF23a9e849D7ac44f, 25.6*1000)); //Otard
-        Recipients.push(Recipient(0xee98a8C53DBcc402BD3a5627849FFc7aEBd8BEfE, 0*1000)); //Nikos_Anasias
+        //Recipients.push(Recipient(0xee98a8C53DBcc402BD3a5627849FFc7aEBd8BEfE, 0*1000)); //Nikos_Anasias
         Recipients.push(Recipient(0x127962b9b5da42856d74ee9d923305dcfae277b8, 38.4*1000)); //slendy1830
         Recipients.push(Recipient(0x6ab75e2577Ced422F57C826Ef60f9F538d16C608, 25.6*1000)); //watukpilek
         Recipients.push(Recipient(0x6DA6D6F50738A6fECa3279604D4d6da3c937Db71, 25.6*1000)); //hypnosse
